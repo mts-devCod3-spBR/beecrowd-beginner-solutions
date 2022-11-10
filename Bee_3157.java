1d6defae7a80e0fc7ee24ac6158bbdad00744c11
Matheus André da Silva <84941202+mts-devCod3-spBR@users.noreply.github.com>
2022-11-10 10:27:24 -0300

    Add files via upload

---
+++ Bee_3157.java
@@ -0,0 +1,29 @@
+/*
+
+QUESTÃO
+Oronob é um garoto muito peculiar. Para ele, apenas a sua opinião importa e nada mais. Durante a pandemia do Coronga Virus, 
+ele usa sua opinião para retaliar o que a ciência diz. Ele então, para impulsionar suas opiniões, 
+criou um bot (robô que gera mensagens automáticas) para disparar seus textos para muitas pessoas.
+Você conhece Oronob, porém, ao contrário dele, você é sensato e sabe que ciência não se refuta com opinião, 
+além também de entender de programação, então decidiu hackear o bot de Oronob para evitar suas falácias.
+
+Pois bem, crie então um algoritmo que muda o texto das opiniões de Oronob para a real verdade.
+
+ENTRADA
+Um texto S com caracteres alfanuméricos e possíveis espaços entre as palavras.
+
+Limites: 0 < |S| <= 280. ;)
+
+SAÍDA
+A verdade.
+
+*/
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        System.out.println("Nao se refuta ciencia com opiniao");
+    }
+}
